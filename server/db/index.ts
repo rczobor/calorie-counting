327950f30b44ec7d72a323097c7eff2a30299d21
Robert Czobor <czobor94@gmail.com>
2024-03-02 09:39:54 +0100

    update deps

--- server/db/index.ts
+++ server/db/index.ts
@@ -4,9 +4,6 @@ import { drizzle } from "drizzle-orm/planetscale-serverless"
 import { env } from "@/env.mjs"
 import * as schema from "./schema"
 
-export const db = drizzle(
-  new Client({
-    url: env.DATABASE_URL,
-  }).connection(),
-  { schema },
-)
+const client = new Client({ url: env.DATABASE_URL })
+
+export const db = drizzle(client, { schema })
