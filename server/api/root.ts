3418c79e55980d3073661649858df9a41bc2d171
Robert Czobor <czobor94@gmail.com>
2024-03-30 23:29:29 +0100

    add all routers to approuter

--- server/api/root.ts
+++ server/api/root.ts
@@ -2,6 +2,8 @@ import { createTRPCRouter } from "@/server/api/trpc"
 import { cookingRouter } from "./routers/cookings"
 import { ingredientRouter } from "./routers/ingredient"
 import { recipeRouter } from "./routers/recipes"
+import { foodRouter } from "./routers/foods"
+import { usedIngredientRouter } from "./routers/used-ingredient"
 
 /**
  * This is the primary router for your server.
@@ -9,9 +11,11 @@ import { recipeRouter } from "./routers/recipes"
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  recipe: recipeRouter,
-  ingredient: ingredientRouter,
   cooking: cookingRouter,
+  food: foodRouter,
+  ingredient: ingredientRouter,
+  recipe: recipeRouter,
+  usedIngredient: usedIngredientRouter,
 })
 
 // export type definition of API
